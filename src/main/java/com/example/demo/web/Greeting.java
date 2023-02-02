03383b7c8ca94f8de497d6136a45a99d6ca82baf
caoyouqian <90828197@qq.com>
2023-02-02 10:31:40 +0800

    springBoot 入门

---
+++ src/main/java/com/example/demo/web/Greeting.java
@@ -0,0 +1,12 @@
+package com.example.demo.web;
+
+import java.util.Objects;
+
+/**
+ * @description:
+ * @author: cyq
+ * @create: 2023/2/1 15:40
+ */
+public record Greeting(long id,String name) {
+
+}
